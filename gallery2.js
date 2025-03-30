058171c2323d14ad281ca3e10031620da26e2de2
Brigid <144155120+brigidone@users.noreply.github.com>
2025-03-29 19:39:40 -0500

    Added responsive header and menu bar.

---
+++ gallery2.js
@@ -0,0 +1,8 @@
+let thumbnails = document.querySelectorAll(".thumbnail");
+let mainImage = document.getElementById("main-image");
+
+for(let i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].addEventListener("onclick", function() {
+        mainImage.src = this.src
+    })
+}
